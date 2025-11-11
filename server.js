73c34e442819ad874a4c5eed4f2b6e50f8a31e30
MamonCh111 <khlybov.vanyaa@gmail.com>
2025-11-11 16:08:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 // ==================== ЗАЩИТА ADMIN.HTML ====================
 app.get('/admin.html', (req, res) => {
     const token = req.cookies?.adminToken;
@@ -791,4 +791,5 @@ process.on('SIGINT', () => {
         console.log('Закрытие соединения с БД');
         process.exit(0);
     });
+
 });
